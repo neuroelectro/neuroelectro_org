7d0c242a523325a2fdb68fe43c705b3a6dabda66
Rick Gerkin <rgerkin@asu.edu>
2013-12-12 14:18:56 -0700

    PEPified neurotree code and added shell utils file shell.py

--- neurotree/pubmed_integration.py
+++ neurotree/pubmed_integration.py
@@ -20,8 +20,8 @@ def pubmed_count_coauthored_papers(author_1, author_2):
     """
     Count number of co-published papers between author_1 and author_2
     
-    co-published defined by number of papers in pubmed where author_1 is first author
-    and author_2 is last author
+    co-published defined by number of papers in pubmed where author_1 is 
+    first author and author_2 is last author
     
     Args:
         author_1 (str): String of first authors name; e.g., Tripathy S
@@ -29,10 +29,13 @@ def pubmed_count_coauthored_papers(author_1, author_2):
     Returns:
         match_count (int): count of numbers of matching pubmed articles 
     """
+
     advisee_author_str = author_1
     adviser_author_str = author_2
-    esearchlink = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=%s[Author] AND (%s[Author])'
-    efetch = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?&db=pubmed&retmode=xml&id=%s"    
+    esearchlink = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?'+\
+                  'db=pubmed&term=%s[Author] AND (%s[Author])'
+    efetch = 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?'+\
+             '&db=pubmed&retmode=xml&id=%s'    
     advisee_last_name = advisee_author_str.split()[0]
     #print advisee_last_name
     adviser_last_name = adviser_author_str.split()[0]
@@ -63,9 +66,11 @@ def pubmed_count_coauthored_papers(author_1, author_2):
                 last_author_last_name = authorList[-1].find("./LastName").text
             except Exception:
                 continue
-            if first_author_last_name == advisee_last_name and last_author_last_name == adviser_last_name:
+            if first_author_last_name == advisee_last_name 
+            and last_author_last_name == adviser_last_name:
                 match_count += 1
-    #print '%s, %s, matching pubs = %d' % (advisee_author_str, adviser_author_str, match_count)
+    #print '%s, %s, matching pubs = %d' % \
+    #               (advisee_author_str, adviser_author_str, match_count)
     return match_count
 
 def compute_neurotree_coauthorship_histo(last_author_node_list):
@@ -74,42 +79,48 @@ def compute_neurotree_coauthorship_histo(last_author_node_list):
     mentors as defined in NeuroTree
     
     Definition of co-published as in: pubmed_count_coauthored_papers() above
+    
+    Args: 
+        last_author_node_list (list): A list of neurotree.models.Node instances.  
     """
-    relationcodes=[1,2]
+
+    relationcodes=[1,2] # 1 is grad student; 2 is postdoc.  
     coauthored_pubs = []
-    for author_node in last_author_node_list:
-        if author_node:
-            author_node_str = '%s %s' %  (author_node.lastname, author_node.firstname[0])
-            parents = [x.node2 for x in author_node.parents.filter(relationcode__in=relationcodes)]
-            for parent_node in parents:
-                parent_node_str = '%s %s' %  (parent_node.lastname, parent_node.firstname[0])
-                coauthor_count = pubmed_count_coauthored_papers(author_node_str, parent_node_str)
-                coauthored_pubs.append(coauthor_count)
+    for a_node in last_author_node_list:
+        if not a_node:
+            continue
+        a_node_str = '%s %s' % (a_node.lastname, a_node.firstname[0])
+        relations = a_node.parents.filter(relationcode__in=relationcodes)
+        parents = [x.node2 for x in relations]
+        for p_node in parents:
+            p_node_str = '%s %s' %  (p_node.lastname, p_node.firstname[0])
+            coauthor_count = pubmed_count_coauthored_papers(a_node_str, 
+                                                            p_node_str)
+            coauthored_pubs.append(coauthor_count)
     return coauthored_pubs
     
-def compute_neurotree_coauthorship_histo_uncond(last_author_node_list, all_author_node_list):
+def compute_neurotree_coauthorship_histo_uncond(last_author_node_list, 
+                                                all_author_node_list):
     """
-    For each author in input list, count how many papers they co-published with
-    each of the authors defined in all_author_node_list
+    For each author in last_author_node_list, count how many papers they 
+    co-published with each of the authors defined in all_author_node_list
     
     Definition of co-published as in: pubmed_count_coauthored_papers() above
     """
-    coauthored_pubs = [[0 for i in range(len(last_author_node_list))] for j in range(len(all_author_node_list))]
-    #coauthored_pubs = [0]*(len(last_author_node_list)*len(all_author_node_list))
-    for i,author_node in enumerate(last_author_node_list):
-        if author_node:
-            author_node_str = '%s %s' %  (author_node.lastname, author_node.firstname[0])
-            print author_node_str
-            for j,parent_node in enumerate(all_author_node_list):
-                if parent_node:
-                    parent_node_str = '%s %s' %  (parent_node.lastname, parent_node.firstname[0])
-                    coauthor_count = pubmed_count_coauthored_papers(author_node_str, parent_node_str)
-                    coauthored_pubs[j][i] = coauthor_count
+
+    coauthored_pubs = [[0 for i in range(len(last_author_node_list))] 
+                       for j in range(len(all_author_node_list))]
+    for i,a_node in enumerate(last_author_node_list):
+        if not a_node:
+            continue
+        a_node_str = '%s %s' %  (a_node.lastname, a_node.firstname[0])
+        print a_node_str
+        for j,p_node in enumerate(all_author_node_list):
+            if not p_node:
+                continue
+            p_node_str = '%s %s' %  (p_node.lastname, p_node.firstname[0])
+            coauthor_count = pubmed_count_coauthored_papers(a_node_str,
+                                                            p_node_str)
+            coauthored_pubs[j][i] = coauthor_count
     return coauthored_pubs
 
-def prog(num,denom):
-    fract = float(num)/denom
-    hyphens = int(round(50*fract))
-    spaces = int(round(50*(1-fract)))
-    sys.stdout.write('\r%.2f%% [%s%s]' % (100*fract,'-'*hyphens,' '*spaces))
-    sys.stdout.flush()  
