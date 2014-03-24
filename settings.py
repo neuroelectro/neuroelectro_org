41b1c6c17677edf56f12f5f152b083c8b371bbb8
stripathy <devnull@localhost>
2014-03-24 16:38:49 -0700

    updates for bulk data download for api

--- settings.py
+++ settings.py
@@ -29,6 +29,8 @@ DATABASES = {
     }
 }
 
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '127.0.0.1:8000', 'localhost:8000', '.kent.pavlab.chibi.ubc.ca', 'kent.pavlab.chibi.ubc.ca.']
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -62,7 +64,7 @@ MEDIA_ROOT = ''
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
