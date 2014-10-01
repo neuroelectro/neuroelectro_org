760af9d3ff673411a0d21a391a2cf128cced2dd3
stripathy <stripat3@gmail.com>
2014-10-01 13:15:28 -0700

    changed logging config in settings.py back to original version

--- settings.py
+++ settings.py
@@ -209,25 +209,20 @@ LOGGING = {
             '()': 'django.utils.log.RequireDebugFalse'
         }
     },
-#     'handlers': {
-#         'mail_admins': {
-#             'level': 'ERROR',
-#             'filters': ['require_debug_false'],
-#             'class': 'django.utils.log.AdminEmailHandler'
-#         },
-#         'default': {
-#             'level': 'ERROR',
-#             'class': 'logging.FileHandler',
-#             'filename': 'logs/debug.log',
-#         },
-#     },
-#     'loggers': {
-#         'django.request': {
-#             'handlers': ['mail_admins', 'default'],
-#             'level': 'ERROR',
-#             'propagate': True,
-#         },
-#     }
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
 }
 
 try:
