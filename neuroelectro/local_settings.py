fee0e0aefc2ea0bf397c47957f4c0a283200d59f
Richard C Gerkin <rgerkin@asu.edu>
2015-08-23 22:11:47 -0700

    Creation of setup.py script and further removal of unnecessary files

--- neuroelectro/local_settings.py
+++ neuroelectro/local_settings.py
@@ -1,7 +1,15 @@
+import os
+from os.path import expanduser
+USER_HOME = expanduser("~")
+NEUROELECTRO_HOME = os.path.join(USER_HOME,'.neuroelectro')
+if not os.path.exists(NEUROELECTRO_HOME):
+    os.mkdir(NEUROELECTRO_HOME)
+DB_HOME = os.path.join(NEUROELECTRO_HOME,'neuroelectro.db')
+
 DATABASES = {
     'default': {
 	'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': 'neuroelectro.db',            
+         'NAME': DB_HOME,            
     }
 }
 
