34b5383f5cc3a3cad8c238d52263ce6294a4ce58
Richard C Gerkin <rgerkin@asu.edu>
2015-08-23 19:44:12 -0700

    Deleted all cruft that the Python API doesn't need

--- helpful_functions/prog.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import sys
-
-def prog(num,denom):
-    fract = float(num)/denom
-    hyphens = int(round(50*fract))
-    spaces = int(round(50*(1-fract)))
-    sys.stdout.write('\r%.2f%% [%s%s]' % (100*fract,'-'*hyphens,' '*spaces))
-    sys.stdout.flush()
