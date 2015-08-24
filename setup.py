f6fc4564a6f2712287a124b2eff5e776dab736ba
Richard C Gerkin <rgerkin@asu.edu>
2015-08-24 12:31:37 -0700

    Updated setup.py

--- setup.py
+++ setup.py
@@ -22,8 +22,8 @@ setup(
 	license='GPL2',
         description='Electrophysiological information from diverse neuron types',
         long_description="The aim of the NeuroElectro Project is to represent structured electrophysiological information from diverse neuron types",
-        setup_requires=['django-simple-history'],
-        install_requires=['django_localflavor_us','django-picklefield','django-simple-history'],
+        setup_requires=['django_simple_history'],
+        install_requires=['django_localflavor_us','django_picklefield','django_simple_history'],
         entry_points = {
             'console_scripts': [
                 'manage_neuroelectro = neuroelectro.manage:main',
