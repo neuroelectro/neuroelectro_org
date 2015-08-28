e7b3e5620ce7cab1544d9042a7e2b0553be3f61a
stripathy <stripat3@gmail.com>
2015-08-28 11:28:01 -0700

    changes to view showing data tables needing curation

--- neuroelectro/views.py
+++ neuroelectro/views.py
@@ -1398,12 +1398,12 @@ def data_table_to_validate_list(request):
         if robot_user in user_list:
             user_list.remove(robot_user)
         dt.curated_by = user_list
-        concept_maps = dt.get_concept_maps()
-        curated_on_dates = []
-        for cm in concept_maps:
-            curated_on = cm.history.latest().history_date
-            curated_on_dates.append(curated_on) 
-        dt.curated_on = max(curated_on_dates)
+#         concept_maps = dt.get_concept_maps()
+#         curated_on_dates = []
+#         for cm in concept_maps:
+#             curated_on = cm.history.latest().history_date
+#             curated_on_dates.append(curated_on) 
+#         dt.curated_on = max(curated_on_dates)
         
     return render('neuroelectro/data_table_to_validate_list.html', {'data_table_list': dts}, request)
 
