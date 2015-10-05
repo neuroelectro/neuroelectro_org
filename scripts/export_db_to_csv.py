bd849ee6398b3e19823030552027c5f18269e34e
Dmitry Tebaykin <dmitry.tebaykin@gmail.com>
2015-10-05 15:36:17 -0700

    Fixed curation interface bug - color resets back to transparent upon cancelling addition of curations to already staged cell. Added some print statements to export_db_to_csv script

--- scripts/export_db_to_csv.py
+++ scripts/export_db_to_csv.py
@@ -2,8 +2,11 @@ __author__ = 'stripathy'
 
 from db_functions.export_db_data import export_db_to_data_frame
 import pandas
+from django.conf import settings
 
 def run():
-    file_name = "article_ephys_metadata_curated.csv"
+    print "Initiating data file creation"
+    file_name = settings.OUTPUT_FILES_DIRECTORY + "article_ephys_metadata_curated.csv"
     df = export_db_to_data_frame()
     df.to_csv(file_name, sep='\t', encoding='utf-8')
+    print "done"
