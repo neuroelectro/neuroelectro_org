0f64630a71f364a894ed3abe742360a033b51167
stripathy <stripat3@gmail.com>
2015-11-06 10:05:44 -0800

    small update to db update shell script

--- db_update_script.sh
+++ db_update_script.sh
@@ -3,5 +3,5 @@ python manage.py runscript update_concept_maps
 python manage.py runscript update_db_summary_fields
 python manage.py runscript database_maintenance
 python manage.py runscript export_db_to_csv
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 
