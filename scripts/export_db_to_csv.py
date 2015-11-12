ac79e70b7617752ce679f0d7e79223aef09b786c
Dmitry Tebaykin <dmitry.tebaykin@gmail.com>
2015-11-12 15:48:07 -0800

    small local changes and fixes

--- scripts/export_db_to_csv.py
+++ scripts/export_db_to_csv.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 def run():
     print "Initiating data file creation"
-    file_name = settings.STATIC_ROOT + "/src/article_ephys_metadata_curated.csv"
+    file_name = settings.OUTPUT_FILES_DIRECTORY + "article_ephys_metadata_curated.csv"
     df = export_db_to_data_frame()
     df.to_csv(file_name, sep='\t', encoding='utf-8')
     print "done"
