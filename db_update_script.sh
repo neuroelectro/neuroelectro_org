9fd977bd1c7ccb506d8018cbd3268ddabc738900
stripathy <stripat3@gmail.com>
2015-11-18 17:29:46 -0800

    attempt at fixing bug in kent nightly cron runs

--- db_update_script.sh
+++ db_update_script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 python manage.py runscript update_concept_maps
 python manage.py runscript update_db_summary_fields
-python manage.py runscript database_maintenance
+#python manage.py runscript database_maintenance # seems to break cron on kent
 python manage.py runscript export_db_to_csv
 python manage.py collectstatic --noinput
 
--- scripts/update_concept_maps.py
+++ scripts/update_concept_maps.py
@@ -7,7 +7,7 @@ def update_cms():
     """Updates neuroelectro NeuronEphysDataMap objects with normalized value and assigns expert validated bool
     to all neuroelectro ConceptMap objects"""
 
-    print 'updating field summaries'
+    print 'updating concept maps for normalization and expert validation'
     normalize_all_nedms()
     assign_expert_validated()
 
