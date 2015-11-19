a19f9747525e0e2b29419c593b34318ff80f95bd
stripathy <stripat3@gmail.com>
2015-11-19 11:36:50 -0800

    adding runscript for adding full text articles from ace generated folders

---
+++ scripts/add_full_texts_to_db.py
@@ -0,0 +1,14 @@
+from article_text_mining.full_text_pipeline import add_full_texts_from_mult_dirs
+__author__ = 'stripathy'
+
+
+def add_full_texts():
+    """Adds full texts from directory to DB based on rules specified in full_text_pipeline"""
+    full_text_dir = '/data/downloaded_html/html/'
+    matching_journ_str = 'Frontiers'
+
+    print 'Adding full texts from %s journals' % matching_journ_str
+    add_full_texts_from_mult_dirs(full_text_dir, matching_journ_str)
+
+def run():
+    add_full_texts()
