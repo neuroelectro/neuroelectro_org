54650ee344e8cf9c27a5560a2a6e50dea85a2cfe
Shreejoy Tripathy <stripat3@gmail.com>
2015-11-26 11:38:18 -0800

    commits kents version of full text adding script to repo

--- scripts/add_full_texts_to_db.py
+++ scripts/add_full_texts_to_db.py
@@ -4,8 +4,8 @@ __author__ = 'stripathy'
 
 def add_full_texts():
     """Adds full texts from directory to DB based on rules specified in full_text_pipeline"""
-    full_text_dir = '/data/downloaded_html/html/'
-    matching_journ_str = 'PLoS'
+    full_text_dir = '/home/stripathy/neuroelectro_full_texts/html/'
+    matching_journ_str = 'Cereb Cortex'
 
     print 'Adding full texts from %s journals' % matching_journ_str
     add_full_texts_from_mult_dirs(full_text_dir, matching_journ_str)
