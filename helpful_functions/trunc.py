a89a1f29dc757d513a2411a16a9e294a6cca5c1e
Shreejoy Tripathy <stripat3@gmail.com>
2016-04-07 18:52:33 -0700

    fixes bug for stringifying floats for data graph views

--- helpful_functions/trunc.py
+++ helpful_functions/trunc.py
@@ -3,7 +3,12 @@ Created on Jul 16, 2014
 
 @author: stripathy
 '''
+
+# copied shamelessly from this StackOverflow answer: http://stackoverflow.com/questions/783897/truncating-floats-in-python/783927
 def trunc(f, n=3):
     '''Truncates/pads a float f to n decimal places without rounding'''
-    slen = len('%.*f' % (n, f))
-    return float(str(f)[:slen])
+    s = '{}'.format(f)
+    if 'e' in s or 'E' in s:
+        return '{0:.{1}f}'.format(f, n)
+    i, p, d = s.partition('.')
+    return float('.'.join([i, (d+'0'*n)[:n]]))
